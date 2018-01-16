b7a9d9af30785ce96a585e4725db6bb467e90c4e
Glitch (various-glue) <none>
2018-01-16 06:04:17 +0000

    ⛈🙍 Checkpoint server.js:603188/213 views/index.html:603188/93 public/style.css:603188/125

--- server.js
+++ server.js
@@ -18,43 +18,12 @@ app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
 
-app.get("/:userInput", function (request, response) {
-  
-  //get userInput
-  var userInput = request.params.userInput; 
-  var endPoint = "https://various-glue.glitch.me/";
-  var check = endPoint + userInput;
-  var reroute = "";
-  var endPoint = "https://various-glue.glitch.me/";
-  var random = "lol";
-
-  //check if user input is a shortURL or a new url
-  var isShortUrl = false;
-  var arrIndex = 0;
-  
-  var test = {
-    "it":"works"
-  }
-  
-  fs.writeFileSync('shortUrl.json', test);
-  
-  if(isShortUrl){
-      //then reroute to its orignal url
-    response.sendFile(reroute);
-  }else if(!isShortUrl){
-    
-    var jsonResponse = {
-      "original_url" : userInput,
-      "short_url" : endPoint + random
-     };
-        
-    //write to shorUrl.json
-    fs.writeFileSync('shortUrl.json', jsonResponse);
-  
-    response.json(jsonResponse);
-    
-  }  
+app.get('/new/:urlToShorten(*)', function (request, response) {
+  var { urlToShorten } = request.params;
+  console.log(urlToShorten);
 });
+
+
 
 
 // listen for requests :)
