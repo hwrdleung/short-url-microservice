466172ff3277ceeded4e1450a5d7f3ab0c993271
Glitch (various-glue) <none>
2018-01-16 01:00:28 +0000

    😂💅 Checkpoint server.js:603188/566

--- server.js
+++ server.js
@@ -5,12 +5,13 @@
 var express = require('express');
 var app = express();
 
-var shortCuts = [];
 // we've started you off with Express, 
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
+
+var urlArr = [];
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (request, response) {
@@ -20,14 +21,33 @@ app.get("/", function (request, response) {
 app.get("/:userInput", function (request, response) {
   
   //get userInput
+  var userInput = request.params.userInput; //this will either be a url or a random number
+  var endPoint = "https://various-glue.glitch.me/";
+  var check = endPoint + userInput;
+
   //check if user input is a shortURL or a new url
+  var isShortUrl = false;
+  var arrIndex = 0;
+  
+  for(var i=0; i<urlArr.length; i++){
+    if(urlArr[i].short_url === check){
+       isShortUrl = true; 
+       arrIndex = i;
+    }
+  }
+  
+  if(isShortUrl){
+      //then reroute to its orignal url
+    response.sendFile();
+  }
   
   //if userinput is a shortURL:
         //reroute user to its respective long url
   
-  //if userinput is a
+  //if userinput is a new url:
+      //return json data
+      //store json data in global array
   
-  var userInput = request.params.userInput;
   var endPoint = "https://various-glue.glitch.me/";
   var random = "shortURL/";
   
@@ -36,7 +56,6 @@ app.get("/:userInput", function (request, response) {
       "short_url" : endPoint + random
   });
   
-  shortCuts.push(jsonResponse);
   
   response.json({
       "original_url" : userInput,
